87a97f958a154fbf87ecbac25a00ae77b7df8a0b
tnunesueno <tnunesueno25@scienceleadership.org>
2025-02-25 13:58:11 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/example/lyricguessr/HelloController.java
    #	src/main/java/com/example/lyricguessr/Lyric.java
    #	src/main/java/com/example/lyricguessr/Song.java
    #	src/main/resources/com/example/lyricguessr/hello-view.fxml

--- src/main/java/com/example/lyricguessr/Playview.java
+++ src/main/java/com/example/lyricguessr/Playview.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Playview {
@@ -12,6 +13,8 @@ private Text lyricText;
 
  @FXML
  private TextField input;
+
+ public ArrayList<String> Array = new ArrayList<String>();
 
  public void initialize() throws Exception {
      Lyric selectedLyrics = Song.randomLyrics();
@@ -22,5 +25,13 @@ private Text lyricText;
 
      lyricText.setText(selectedLyrics.lyricArray.get(lyricNUm));
 
+     // adds all lyrics from song to this class's own array that can be used in other methods
+     for (int l = 0; l<selectedLyrics.getLyricArray().size(); l++){
+         if (!selectedLyrics.getLyricArray().get(l).isEmpty()){
+         Array.add(selectedLyrics.getLyricArray().get(l));
+         System.out.println("added line: "+ Array.get(l));} else {
+             System.out.println("line is empty, moving on");
+         }
      }
  }
+}
